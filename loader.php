f3791442fe029d7b9d4d7cc0b40e476d9105cb58
Седенков Николай <n.sedenkov@maximaster.ru>
2018-12-21 14:42:35 +0300

    Изменена регистрация автозагрузчика - без анонимной функции

--- loader.php
+++ loader.php
@@ -1,23 +1,27 @@
 <?php
 
-//autoload psr-0
-spl_autoload_register(function ($className) {
-    $includeNamespace = 'Sprint\\Migration';
-    $includePath = __DIR__ . '/classes';
+if (!function_exists('sprint_loader')) {
+    function sprint_loader($className)
+    {
+        $includeNamespace = 'Sprint\\Migration';
+        $includePath = __DIR__ . '/classes';
 
-    if ($includeNamespace . '\\' === substr($className, 0, strlen($includeNamespace . '\\'))) {
-        $fileName = '';
-        if (false !== ($lastNsPos = strripos($className, '\\'))) {
-            $namespace = substr($className, 0, $lastNsPos);
-            $className = substr($className, $lastNsPos + 1);
-            $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-        }
-        $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+        if ($includeNamespace . '\\' === substr($className, 0, strlen($includeNamespace . '\\'))) {
+            $fileName = '';
+            if (false !== ($lastNsPos = strripos($className, '\\'))) {
+                $namespace = substr($className, 0, $lastNsPos);
+                $className = substr($className, $lastNsPos + 1);
+                $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+            }
+            $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
 
-        $fileName = ($includePath !== null ? $includePath . DIRECTORY_SEPARATOR : '') . $fileName;
-        if (is_readable($fileName)) {
-            /** @noinspection PhpIncludeInspection */
-            require $fileName;
+            $fileName = ($includePath !== null ? $includePath . DIRECTORY_SEPARATOR : '') . $fileName;
+            if (is_readable($fileName)) {
+                /** @noinspection PhpIncludeInspection */
+                require $fileName;
+            }
         }
     }
-});
+
+    spl_autoload_register('sprint_loader');
+}
